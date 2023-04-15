d6b4399191457ae0f4727f21f14a08fa6ef47197
NagiPragalathan <kk2069037@gmail.com>
2023-04-15 09:20:05 +0530

    last commit

--- builder/urls.py
+++ builder/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .Routes import NoCodeViews, BlogViews,common
+from .Routes import NoCodeViews, BlogViews, common, AI_Functions
 from WebpageBuilderDjango import settings
 from django.conf.urls.static import static
 
-#Initilizes........................
+# Initilizes........................
 
 urlpatterns = []
+
+
 def Make_Join(Componets):
     OutComponets = []
     for i in Componets:
@@ -17,12 +19,12 @@ def Make_Join(Componets):
 # Paths.............................
 
 NoCodeMaker = [
-    path('view_pages',NoCodeViews.index, name='home'),
-    path('',common.home, name='home'),
-    path('home',common.home, name='home'),
-    path('automation',common.automation, name='automation'),
-    path('blog',common.blog, name='blog'),
-    path('compiler',common.compiler, name='compiler'),
+    path('view_pages', NoCodeViews.index, name='home'),
+    path('', common.home, name='home'),
+    path('home', common.home, name='home'),
+    path('automation', common.automation, name='automation'),
+    path('blog', common.blog, name='blog'),
+    path('compiler', common.compiler, name='compiler'),
     path('add', NoCodeViews.addPage, name="addpage"),
     path('edit/<id>', NoCodeViews.editPage, name="editpage"),
     path('page/create', NoCodeViews.savePage, name="create_page"),
@@ -31,19 +33,22 @@ NoCodeMaker = [
 ]
 
 BlogBuilder = [
-    path('list_blog',BlogViews.list_blog),
-    path('list_edit_blog',BlogViews.list_edit_blog),
-    path('view_blog/<str:pk>',BlogViews.view_blog),
-    path('edit_blog/<str:pk>',BlogViews.edit_blog),
-    path('blog_edit',BlogViews.blog_edit),
-    path('save_blog',BlogViews.save_blog),
-    path('delete_blog',BlogViews.delete_blog),
-    path('edit_blog/save_edit_blog/<int:pk>',BlogViews.save_edit_blog),
+    path('list_blog', BlogViews.list_blog),
+    path('list_edit_blog', BlogViews.list_edit_blog),
+    path('view_blog/<str:pk>', BlogViews.view_blog),
+    path('edit_blog/<str:pk>', BlogViews.edit_blog),
+    path('blog_edit', BlogViews.blog_edit),
+    path('save_blog', BlogViews.save_blog),
+    path('delete_blog', BlogViews.delete_blog),
+    path('edit_blog/save_edit_blog/<int:pk>', BlogViews.save_edit_blog),
 ]
 
-
+AI_functions = [
+    path('Code_scriping', AI_Functions.Code_scriping),
+    path('Error_Solver', AI_Functions.Error_Solver),
+]
 
 # Add Paths Together..............
 
-urlpatterns.extend(Make_Join([NoCodeMaker,BlogBuilder]))
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns.extend(Make_Join([NoCodeMaker, BlogBuilder, AI_functions]))
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
