102b56d0e6cedeeb12ebe8d47b3e51428129a1c8
NagiPragalathan <kk2069037@gmail.com>
2023-04-15 16:36:28 +0530

    sample commit

--- builder/Routes/NoCodeViews.py
+++ builder/Routes/NoCodeViews.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.http import JsonResponse
 from ..models import Pages
 from django.core.serializers import serialize
